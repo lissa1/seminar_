4ca69398540f6ca911d2f3c942b6c46c56e76cd8
liza <e-namestnikova@inbox.ru>
2022-06-26 19:31:26 +0700

    add task 4

---
+++ 77/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введите 1 число ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите 2 число ");
+double num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 * num1 == num2 || num2 * num2 == num1)
+{
+    Console.WriteLine("квадрат есть");
+}
+else
+{
+    Console.WriteLine("квадратa нет");
+}
