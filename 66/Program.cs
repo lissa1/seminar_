1d2f79e6cfb559ba2a683549fae0394659d99b1b
liza <e-namestnikova@inbox.ru>
2022-06-26 19:30:51 +0700

    add task 2

---
+++ 66/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("введитете 1 число ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write("введитете 2 число ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+int num3 = num1 % num2;
+if (num3 == 0)
+{
+    Console.WriteLine("кратное");
+}
+else
+{
+    Console.WriteLine("некратное, остаток " + num3);
+}
