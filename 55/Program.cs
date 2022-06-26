68b5839352bffa7942bd05433a6756579e2fa5f0
liza <e-namestnikova@inbox.ru>
2022-06-26 19:29:49 +0700

    add task 3

---
+++ 55/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите 1 число ");
+double num1 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 % 7 == 0 && num1 % 23 == 0)
+{
+    Console.WriteLine("кратно");
+}
+else 
+{
+    Console.WriteLine("не кратно");
+}
